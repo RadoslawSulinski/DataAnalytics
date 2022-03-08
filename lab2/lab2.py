1e5c2a1ed225e70837cebb536b1a60ffc55ee252
Radosław Suliński <radoslaw.sulinski@gmail.com>
2022-03-08 11:50:34 +0100

    Add Exercise 2 for lab 2

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,6 +1,7 @@
 from cmdstanpy import CmdStanModel
 import matplotlib.pyplot as plt
 import pandas as pd
+from random import shuffle
 
 
 F = 8
@@ -18,3 +19,27 @@ for series in stan_data:
     plt.hist(stan_data[series])
     plt.title(series)
     plt.show()
+
+# Exercise 2
+models = {i: CmdStanModel(stan_file=f'code_{i}.stan') for i in (2, 3)}
+
+# Use N=0 for both models
+stan_y = [0]*8 + [1]*7 + [2]
+shuffle(stan_y)
+stan_data = {'N': 16,
+             'y': stan_y}
+try:
+    samples = {i: models[i].sample(stan_data) for i in models}
+except RuntimeError:
+    print("Value out of bounds, runtime error is thrown")
+    samples = {i: models[i].sample({'N': 16, 'y': [0]*8 + [1]*8}) for i in models}
+for sample in samples.values():
+    theta = sample.stan_variable('theta')
+    summary = sample.summary()
+    plt.hist(theta, bins=20)
+    plt.axvline(summary['5%']['theta'], color='r')
+    plt.axvline(summary['50%']['theta'], color='g')
+    plt.axvline(summary['95%']['theta'], color='b')
+    plt.axvline(theta.mean(), color='y')
+    plt.show()
+
